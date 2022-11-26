3e94f1f4bad204c72275f470842258efce54aec0
potocnikvid <vidpotocnik0@gmail.com>
2022-11-26 21:20:03 +0100

    google maps

--- components/components/Video.js
+++ components/components/Video.js
@@ -30,12 +30,21 @@ export default function Video() {
                 scrolling="no"
                 src="https://kuula.co/share/collection/79Z2b?logo=-1&info=0&fs=0&vr=1&sd=1&gyro=0&autorotate=0.67&autop=5&autopalt=1&thumbs=3&alpha=0.60&inst=0&keys=0"
             ></iframe> */}
-            <iframe
+            {/* <iframe
                 className="ku-embed absolute z-10 w-auto min-w-full min-h-full max-w-none"
                 frameBorder="0"
                 allowFullScreen
                 scrolling="no"
                 src="https://kuula.co/share/collection/79MSp?logo=-1&info=0&fs=0&vr=0&sd=1&gyro=0&autopalt=1&thumbs=3&inst=0&keys=0"
+            ></iframe> */}
+            <iframe
+                className="ku-embed absolute z-10 w-auto min-w-full min-h-full max-w-none"
+                src="https://www.google.com/maps/embed?pb=!4v1669493840616!6m8!1m7!1sCAoSLEFGMVFpcFBjLU1pTU9IQi1BM3J5RHkxQUQxVnBYbmtMRTEyd3ZLLV9QNUN3!2m2!1d46.07527017919635!2d14.48439221530951!3f55.82!4f1.8299999999999983!5f0.7820865974627469"
+                // width="800"
+                // height="600"
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
             ></iframe>
         </div>
     );
